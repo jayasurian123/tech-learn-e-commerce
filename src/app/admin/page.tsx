8b879b7422cb716699e47582247753c7369691ac
Jayasurian Makkoth <jayasurian123@yahoo.com>
2024-05-31 17:08:07 +0200

    sales card added

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,37 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
 const AdminPage = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-4 mx-2">
+      <DashboardCard
+        title="Sales"
+        desc="sales desc"
+        content="I am sales data"
+      />
+    </div>
+  );
 };
+
+type DashboardCardProps = { title: string; desc: string; content: string };
+
+function DashboardCard({ title, desc, content }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{desc}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{content}</p>
+      </CardContent>
+    </Card>
+  );
+}
 
 export default AdminPage;
