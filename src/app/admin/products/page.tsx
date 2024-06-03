b96f3379ead06366709af9c7c66dfc78c79b1080
Jayasurian Makkoth <jayasurian123@yahoo.com>
2024-06-03 11:18:33 +0200

    product table

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,5 +1,41 @@
-const ProductsPage = () => {
-  return <div>Products Page</div>;
+import { Button } from '@/components/ui/button';
+import { Table, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+
+import Link from 'next/link';
+import PageHeader from '../_components/PageHeader';
+
+const AdminProductsPage = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between gap-4">
+        <PageHeader>Header</PageHeader>
+        <Button asChild>
+          <Link href="/admin/products/new">Add Product</Link>
+        </Button>
+      </div>
+      <ProductsTable />
+    </>
+  );
 };
 
-export default ProductsPage;
+function ProductsTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Avaialable for purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+    </Table>
+  );
+}
+
+export default AdminProductsPage;
