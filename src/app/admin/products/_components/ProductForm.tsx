898bdeb55a66d3b6ead0714c81808b1b2b008e8f
Jayasurian Makkoth <jayasurian123@yahoo.com>
2024-06-04 16:06:26 +0200

    eidt form

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -5,19 +5,29 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { formatCurrency } from '@/lib/formatter';
+import { Product } from '@prisma/client';
+import Image from 'next/image';
 import { useState } from 'react';
 import { useFormState, useFormStatus } from 'react-dom';
 import { addProduct } from '../../_actions/products';
 
-const ProductForm = () => {
+const ProductForm = ({ product }: { product?: Product | null }) => {
   const [errorState, formAction] = useFormState(addProduct, {});
-  const [priceInCents, setPriceInCents] = useState<number>();
+  const [priceInCents, setPriceInCents] = useState<number | undefined>(
+    product?.priceInCents,
+  );
 
   return (
     <form action={formAction} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required />
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          required
+          defaultValue={product?.name || ''}
+        />
         {errorState.name && (
           <div className="text-destructive">{errorState.name}</div>
         )}
@@ -42,21 +52,48 @@ const ProductForm = () => {
         </div>
         <div className="space-y-2">
           <Label htmlFor="description">Description</Label>
-          <Textarea id="description" name="description" required />
+          <Textarea
+            id="description"
+            name="description"
+            required
+            defaultValue={product?.description || ''}
+          />
           {errorState.description && (
             <div className="text-destructive">{errorState.description}</div>
           )}
         </div>
         <div className="space-y-2">
           <Label htmlFor="file">File</Label>
-          <Input type="file" id="file" name="file" required />
+          <Input
+            type="file"
+            id="file"
+            name="file"
+            required={product === null}
+          />
+          {product !== null && (
+            <div className="text-muted-foreground">{product?.filePath}</div>
+          )}
           {errorState.file && (
             <div className="text-destructive">{errorState.file}</div>
           )}
         </div>
         <div className="space-y-2">
           <Label htmlFor="image">Image</Label>
-          <Input type="file" id="image" name="image" required />
+          <Input
+            type="file"
+            id="image"
+            name="image"
+            required={product === null}
+          />
+          {product !== null && (
+            <Image
+              className="object-contain"
+              src={`/${product?.imagePath}`}
+              alt="preview image"
+              width={200}
+              height={200}
+            />
+          )}
           {errorState.image && (
             <div className="text-destructive">{errorState.image}</div>
           )}
